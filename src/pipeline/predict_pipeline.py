bdca05e13b95d09b1ee41d60449d3d9026b9ed99
Tanjina <tanjinaproma@gmail.com>
2023-05-10 16:21:10 +0600

    modified Prediction Pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
@@ -9,8 +10,8 @@ class PredictPipeline:
         pass
     def predict(self, features):
         try:
-            model_path = 'artifacts\model.pkl'
-            preprocessor_path = 'artifacts\preprocessor.pkl'
+            model_path = os.path.join('artifacts\model.pkl')
+            preprocessor_path = os.path.join('artifacts\preprocessor.pkl')
             model = load_object(file_path=model_path)
             preprocessor= load_object(file_path=preprocessor_path)
             data_scaled = preprocessor.transform(features)
@@ -50,12 +51,12 @@ class CustomData:
         try:
             custom_data_input_dict = {
                 "gender": [self.gender],
-                "race_ethnicity": [self.race_ethnicity],
-                "parental_level_of_education": [self.parental_level_of_education],
+                "race/ethnicity": [self.race_ethnicity],
+                "parental level of education": [self.parental_level_of_education],
                 "lunch": [self.lunch],
-                "test_preparation_course": [self.test_preparation_course],
-                "reading_score": [self.reading_score],
-                "writing_score": [self.writing_score],
+                "test preparation course": [self.test_preparation_course],
+                "reading score": [self.reading_score],
+                "writing score": [self.writing_score],
             }
 
             return pd.DataFrame(custom_data_input_dict)
